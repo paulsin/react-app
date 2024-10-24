8133de07a95dbb337a4c4b85be809ea0447db3e7
paulsin <paulsin91@gmail.com>
2024-10-24 09:57:56 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import  './custom/custom.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React PAULSIN polachan puliparamban
-        </a>
-      </header>
-    </div>
+
+    <nav class="navbar navbar-expand-md">
+		  <a class="navbar-brand" href="#">Logo</a>
+		  <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+			  <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="main-navigation">
+			  <ul class="navbar-nav">
+				  <li class="nav-item">
+				  	<a class="nav-link" href="#">Home</a>
+				  </li>
+				  <li class="nav-item">
+					  <a class="nav-link" href="#">About</a>
+				  </li>
+				  <li class="nav-item">
+					  <a class="nav-link" href="#">Contact</a>
+				  </li>
+			  </ul>
+		  </div>
+	  </nav>
+
   );
 }
 
