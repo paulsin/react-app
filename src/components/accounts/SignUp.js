e334a02338979f2a2f65d7848d51f585fc68da81
paulsin <paulsin91@gmail.com>
2024-11-07 22:44:22 +0530

    input clear

--- src/components/accounts/SignUp.js
+++ src/components/accounts/SignUp.js
@@ -20,27 +20,27 @@ function SignUp() {
 
             <div class="mb-3 mt-3">
               <label for="name">Name:</label>
-              <input type="name" class="form-control" id="name" placeholder="Enter name" name="name" />
+              <input type="name" class="form-control" id="name" placeholder="Enter name" name="name" required/>
             </div>
 
             <div class="mb-3 mt-3">
               <label for="mobile">Mobile:</label>
-              <input type="mobile" class="form-control" id="mobile" placeholder="Enter mobile number" name="mobile" />
+              <input type="number" pattern="[0-9]*" class="form-control" id="mobile" placeholder="Enter mobile number" name="mobile" required/>
             </div>
 
             <div class="mb-3 mt-3">
               <label for="email">Email:</label>
-              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" />
+              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required/>
             </div>
 
             <div class="mb-3 mt-3">
               <label for="password">Password:</label>
-              <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" />
+              <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required/>
             </div>
 
             <div class="mb-3">
               <label for="confirmPassword">Confirm password:</label>
-              <input type="confirmPassword" class="form-control" id="confirmPassword" placeholder="Repeat password" name="confirmPassword" />
+              <input type="password" class="form-control" id="confirmPassword" placeholder="Repeat password" name="confirmPassword" required/>
             </div>
 
             <div class="form-check mb-3">
