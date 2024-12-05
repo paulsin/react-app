be93adde1cbef642c78e3c8c4fc4997bfc23b4fb
paulsin <paulsin91@gmail.com>
2024-12-05 13:16:02 +0530

    axios

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,4 +1,4 @@
-const Url = 'http://localhost:3000/' // For localhost
-//const Url = 'https://haberoceanstock.com/backend/' // For website
+//const Url = 'http://localhost:3000/' // For localhost
+const Url = 'https://haberoceanstock.com/backend/' // For website
 
 export {Url}        
