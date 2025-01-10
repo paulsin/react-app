abd67c1fef0cfe14351448ad97d6778c96e742c2
paulsin <paulsin91@gmail.com>
2025-01-10 20:25:41 +0530

    Image uploading

--- src/components/public/Home.js
+++ src/components/public/Home.js
@@ -27,15 +27,12 @@ function Home() {
   const StateType=(e) =>{
     var options = e.target.options;
     //alert(options)
-    /*
     var value = [];
     for (var i = 0, l = options.length; i < l; i++) {
       if (options[i].selected) {
         value.push(options[i].value);
       }
     }
-      */
-
   }
   function getStates() {
     // alert("anu");
