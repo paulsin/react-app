11f5a55c24332f6198c38806e2f421cb729feb77
User8 <User8@SCHNELLPACK.com>
2025-01-11 17:27:56 +0530

    jk

--- src/components/public/Home.js
+++ src/components/public/Home.js
@@ -8,14 +8,18 @@ import { MultiSelect } from "react-multi-select-component";
 import axios from "axios";
 import { Url } from "../../constants/global";
 import Select from 'react-select';
-
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { IoLocation } from "react-icons/io5";
+import { CgMail } from "react-icons/cg";
 
 function Home() {
   const [selectedpropertytype, setSelectedPropertyType] = useState([]);
   const [selectedstatetype, setSelectedStateType] = useState([]);
   const [selecteddistricttype, setSelectedDistrictType] = useState([]);
   const [selectedtowntype, setSelectedTownType] = useState([]);
-  const [selectedstateonchangevalue, setSelectedstateOnchangevalue] = useState();
+  const [selectedstateonchangevalue, setSelectedstateOnchangevalue] = useState([]);
+  const [selecteddistrictonchangevalue, setSelecteddistrictOnchangevalue] = useState([]);
 
   const propertytype_options = [
     { label: "Flat", value: "flat" },
@@ -26,24 +30,42 @@ function Home() {
   let state_options = []
   let district_options = []
   let town_options = []
-
+  let district_values=[]
+  let town_values=[]
   const StateType = (event) => {
-   
-    // var optionss = event.target.options;
-    alert(event)
-    let statevalues=[]
-    // alert(event._id)
-  
-    statevalues.push(event)
-    // alert(statevalues)
-    setSelectedstateOnchangevalue(event)
-    statevalues.map(state=>{
-       alert(state._id)
+    // alert(event)
+    selecteddistricttype.map(key1=>{
+      // alert(key1.value);
+      // alert(key1.label)
+      event.map(key=>{
+        // alert(key.value)
+        if(key1.stateID===key.value)
+        {
+          //alert("haiiii")
+          district_values.push({ value: key1.value, label: key1.label });
+        }
+      })
     })
-
-
+    // alert(district_values)
+    setSelectedstateOnchangevalue(district_values);
   }
 
+  const DistrictType = (event1) => {
+    selectedtowntype.map(key2=>{
+      // alert(key2.value);
+      // alert(key2.label)
+      event1.map(key3=>{
+        //  alert(key3.value)
+        if(key2.districtID===key3.value)
+        {
+          // alert("haiiii")
+          town_values.push({ value: key2.value, label: key2.label });
+        }
+      })
+    })
+    // alert(district_values)
+    setSelecteddistrictOnchangevalue(town_values);
+  }
 
   function getStates() {
     // alert("anu");
@@ -57,7 +79,7 @@ function Home() {
           // alert(data1._id)    
           state_options.push({ value: data1._id, label: data1.stateName });
       });
-      // alert(state_options)
+         // setSelectedstateOnchangevalue(district_values) // alert(state_options)
       setSelectedStateType(state_options);
     })
   }
@@ -69,7 +91,7 @@ function Home() {
     .then((res1) => {
       res1.data.map(data2 => {
           // alert(data.stateID);
-        district_options.push({ value: data2._id, label: data2.districtName });
+        district_options.push({ value: data2._id, label: data2.districtName, stateID: data2.stateID});
       });
       setSelectedDistrictType(district_options);
     })
@@ -81,7 +103,7 @@ function Home() {
     )
     .then((res2) => {
       res2.data.map(data3 => {
-        town_options.push({ value: data3._id, label: data3.townName });
+        town_options.push({ value: data3._id, label: data3.townName, districtID: data3.districtID });
       });
       setSelectedTownType(town_options);
     })
@@ -93,8 +115,8 @@ function Home() {
   }, []);
   
   return(
-    <div>
-      <NavbarPublic />
+      <div>
+        <NavbarPublic />
         <header class="page-header header container-fluid-full mx-auto p-3">
         
           <div className='container .d-block' id="searchpropclass" >
@@ -150,7 +172,7 @@ function Home() {
                     labelledBy="Select"
                   /> */}
                  
-                    <Select  id="selectboxcolor" isMulti={true} options={selectedstatetype}  value={selectedstateonchangevalue} onChange={StateType}> 
+                    <Select  id="selectboxcolor" isMulti={true} options={selectedstatetype}  onChange={StateType}> 
                     </Select>   
                     
                 </div>
@@ -171,12 +193,16 @@ function Home() {
                   <label htmlFor=""><b>District</b></label>
                 </div>
                 <div className="col-md-3">
-                  <MultiSelect
-                    options={selecteddistricttype}
+
+                  <Select  id="selectboxcolor" isMulti={true} options={selectedstateonchangevalue} onChange={DistrictType} > 
+                  </Select>  
+                  {/* <MultiSelect
+                    options={selectedstateonchangevalue}
+                    
                     // value={selecteddistricttype}
                     onChange={setSelectedDistrictType}
                     labelledBy="Select"
-                  />
+                  /> */}
                     {/* <select className="form-control">
                       <option>Ernakulam</option>
                       <option>Thrissur</option>
@@ -186,12 +212,14 @@ function Home() {
                   <label htmlFor=""><b>Town</b></label>
                 </div>
                 <div className="col-md-3">
-                  <MultiSelect
-                    options={selectedtowntype}
+                <Select  id="selectboxcolor" isMulti={true} options={selecteddistrictonchangevalue} onChange={DistrictType} > 
+                </Select>
+                  {/* <MultiSelect
+                    options={selecteddistrictonchangevalue}
                     // value={selectedtowntype}
                     onChange={setSelectedTownType}
                     labelledBy="Select"
-                  />
+                  /> */}
     
                     {/* <select className="form-control" data-mdb-select-init multiple>
                       <option>Angamaly</option>
@@ -255,72 +283,201 @@ function Home() {
           </div>
         </header>
 
-
-
-	          {/* <h1>Welcome to the Home</h1>
-	          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.</p>
-
-            <button class="btn btn-outline-secondary btn-lg">Tell Me More!</button> */}
-
-          {/* </div>
-        </div> */}
-     
       
-      <div class="container features" >
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="container features" >
+            <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                    <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                    </div>
+                
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>
+              
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
                 <h3 class="feature-title">Lorem ipsum</h3>
                 <img src={background} class="img-fluid" />
                 <div class="container mx-auto p-3 text-center" id="properties_container">
                   <div class="row row-cols-2" >
-                    <div class="col" id="properties1">Column1</div>
-                    <div class="col" id="properties2">Column2</div>
-                    <div class="col" id="properties2">Column3</div>
-                    <div class="col" id="properties1">Column4</div>
-                    <div class="col" id="properties1">Column5</div>
-                    <div class="col" id="properties2">Column6</div>
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
                   </div>
-              
                 </div>
-                <button className='btn' id="searchbuttoninhome">More Details</button>
-            </div>
-            
-            <div class="col-lg-4 col-md-4 col-sm-12">
+                  <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
                 <h3 class="feature-title">Lorem ipsum</h3>
                 <img src={background} class="img-fluid" />
                 <div class="container mx-auto p-3 text-center" id="properties_container">
                   <div class="row row-cols-2" >
-                    <div class="col" id="properties1">Column1</div>
-                    <div class="col" id="properties2">Column2</div>
-                    <div class="col" id="properties2">Column3</div>
-                    <div class="col" id="properties1">Column4</div>
-                    <div class="col" id="properties1">Column5</div>
-                    <div class="col" id="properties2">Column6</div>
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
                   </div>
-              
                 </div>
                 <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>   
             </div>
-            
-            
-            <div class="col-lg-4 col-md-4 col-sm-12">
-              <h3 class="feature-title">Get in Touch!</h3>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Name" name="" />
+            <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
               </div>
-              <div class="form-group">
-                  <input type="email" class="form-control" placeholder="Email Address" name="email" />
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
               </div>
-              <div class="form-group">
-                  <textarea class="form-control" rows="4"></textarea>
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>   
+            </div>
+            <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
               </div>
-              <input type="submit" class="btn btn-secondary btn-block" value="Send" name=""></input>
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-12 mx-auto p-4">
+                <h3 class="feature-title">Lorem ipsum</h3>
+                <img src={background} class="img-fluid" />
+                <div class="container mx-auto p-3 text-center" id="properties_container">
+                  <div class="row row-cols-2" >
+                      <div class="col" id="properties1">Column1</div>
+                      <div class="col" id="properties2">Column2</div>
+                      <div class="col" id="properties2">Column3</div>
+                      <div class="col" id="properties1">Column4</div>
+                      <div class="col" id="properties1">Column5</div>
+                      <div class="col" id="properties2">Column6</div>
+                  </div>
+                </div>
+                <button className='btn' id="searchbuttoninhome">More Details</button>
+              </div>   
             </div>
         </div>
-    </div>
-
-
-    </div>
+    
+        <div class="footer">  
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-md-3 col-sm-12 mx-auto p-3">
+                <h4>VirtuneRealtors</h4> <br/>
+                <p>A realestate company with more than 10 years of experience and satisfied customers from all over the world, which will help you to find your dream home in an easy way</p>
+              </div> 
+              <div class="col-lg-3 col-md-3 col-sm-12 mx-auto p-3">
+                  <h4>Mapsite</h4><br/>
+                  <a id="linktagsinfooter" href="">Home</a><br/>
+                  <a id="linktagsinfooter" href="">About us</a><br/>
+                  <a id="linktagsinfooter" href="">Ernakulam</a><br/>
+                  <a id="linktagsinfooter" href="">Thrissur</a><br/>
+                  <a id="linktagsinfooter" href="">Advertise</a><br/>
+                  <a id="linktagsinfooter" href="">Become Our Franchisee</a><br/>
+                  <a id="linktagsinfooter" href="">Contact us</a>
+              </div> 
+              <div class="col-lg-3 col-md-3 col-sm-12 mx-auto p-3">
+                  <h4>Contact</h4><br/>
+                  <a id="linktagsinfooter" href=""><FaPhoneAlt />&nbsp;&nbsp;&nbsp;+91 - 9497 811 259</a><br/>
+                  <a id="linktagsinfooter" href=""><FaPhoneAlt />&nbsp;&nbsp;&nbsp;+91 - 9048 224 630</a><br/>
+                  <a id="linktagsinfooter" href=""><IoLogoWhatsapp />&nbsp;&nbsp;&nbsp;+91 - 8289 844 344</a><br/>
+                  <a id="linktagsinfooter" href=""><CgMail />&nbsp;&nbsp;&nbsp;virtunerealtors@gmail.com</a><br/>
+                  <a id="linktagsinfooter" href=""><IoLocation />&nbsp;&nbsp;&nbsp;Kochi, Kerala, India</a><br/> 
+              </div> 
+              <div class="col-lg-3 col-md-3 col-sm-12 mx-auto p-3">
+                <h4 class="feature-touch">Subscribe</h4>
+                <br/>
+                <h6>Get our newsletter</h6>
+                <div class="form-group">
+                  <input type="email" class="form-control" placeholder="Enter Email Address" name="email" />
+                </div>
+                <input type="submit" class="btn btn-secondary btn-block" value="Submit" name=""></input>
+              </div>
+            </div>
+          </div>
+          <div className="row mx-auto p-3 text-center" >
+            <div className="col-md"> 
+            </div>
+            <div className="col-md"> 
+              <h6 id="copyrightid">© Copyright 2025 VirtuneRealtors - All rights reserved.</h6>   
+            </div>
+            <div className="col-md"> 
+            </div>
+          </div> 
+        </div>
+      </div>
 
     )
 }
