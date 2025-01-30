6198e3a50dee85fc532437e2ba0ef4e3c5a357bf
User8 <User8@SCHNELLPACK.com>
2025-01-30 17:48:12 +0530

    fghj

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,6 +1,6 @@
- const Url = 'http://localhost:3000/backend/' // For localhost
+ const Url = 'http://localhost:3000/backend/' ;// For localhost
 //const Url = 'https://haberoceanstock.com/backend/' // For website
-
+const Noimage="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
 const propertyTypes = [
     { value: '', label: 'Property type' },
     { value: 'House', label: 'House' },
